b18792e2e7c00b0a3e2c060da60f7d061fa5b59f
jneilliii <jneilliii+github@gmail.com>
2021-05-03 21:29:27 -0400

    add static webcam image that can be embedded in Custom Action button using the command `[!WEB]http://localhost/plugin/octodashcompanion/static/html/webcam.html`.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ plugin_package = "octoprint_octodashcompanion"
 plugin_name = "OctoDash Companion"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.0.2dev1"
+plugin_version = "0.0.2"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
